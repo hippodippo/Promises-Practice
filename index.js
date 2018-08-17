c011381bac5e43df2ea7a928c085c1a553d3b08d
Kaycee Ingram <ingramkaycee@gmail.com>
2018-08-17 10:02:47 -0500

    few changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Kaycee Ingram
-// Writing our own Promises to gain a real understanding.
+// Writing our own Promise
 
 const request = require('request');
 
@@ -13,4 +13,4 @@ function getData(api) {
 
 getData('https://swapi.co/api/people/1/')
   .then(data => console.log(data)) // do stuff with data (if succeeds).
-  .catch(error => console.error(error)); // do stuff with error (if fails).
+  .catch(error => console.error('Uh oh... ', error)); // do stuff with error (if fails).
