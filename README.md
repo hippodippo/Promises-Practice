43da9b090dd1d6713dce3edb78f82e906fafa332
Kaycee Ingram <ingramkaycee@gmail.com>
2018-08-17 10:30:29 -0500

    added async and await

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ## Learning Promises
 
 Check out this [awesome article on Promises](https://developers.google.com/web/fundamentals/primers/promises)
+
+And this [awesome article on Async and Await](https://developers.google.com/web/fundamentals/primers/async-functions)
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Kaycee Ingram
 // Writing our own Promise
+// ------------------------
 
 const request = require('request');
+const axios = require('axios');
 
 function getData(api) {
   return new Promise((resolve, reject) => {
@@ -14,3 +16,15 @@ function getData(api) {
 getData('https://swapi.co/api/people/1/')
   .then(data => console.log(data)) // do stuff with data (if succeeds).
   .catch(error => console.error('Uh oh... ', error)); // do stuff with error (if fails).
+
+// --------------------------------------------
+
+// Using Async and Await
+// ----------------------
+
+async function getData(url) {
+  const res = await axios.get(url);
+  console.log(res.data);
+}
+
+getData('https://swapi.co/api/people/1/');
